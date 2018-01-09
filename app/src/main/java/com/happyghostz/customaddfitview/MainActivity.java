f4bb9cb83764a396862018d95eede3ad86109a20
happyghost <zcphappyghost@163.com>
2018-01-09 21:21:59 +0800

    fristcreat

---
+++ app/src/main/java/com/happyghostz/customaddfitview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.happyghostz.customaddfitview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
